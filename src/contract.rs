83fcd81ec8b11606e128a38226ee0bbe9abb1357
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2025-01-08 17:04:10 -0500

    add contract entry point

--- src/contract.rs
+++ src/contract.rs
@@ -24,12 +24,21 @@ pub fn instantiate(
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: ExecuteMsg,
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    use ExecuteMsg::*;
+
+    match msg {
+        BuyStorage {
+            for_address,
+            duration,
+            bytes,
+            payment_denom,
+        } => execute::buy_storage(deps, env, info, for_address, duration, bytes, payment_denom),
+    }
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -37,5 +46,21 @@ pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
     unimplemented!()
 }
 
+mod execute {
+    use crate::error::ContractError;
+    use cosmwasm_std::{DepsMut, Env, MessageInfo, Response};
+
+    pub fn buy_storage(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        for_address: String,
+        duration: i64,
+        bytes: i64,
+        payment_denom: String,
+    ) -> Result<Response, ContractError> {
+        unimplemented!()
+    }
+}
 #[cfg(test)]
 mod tests {}
